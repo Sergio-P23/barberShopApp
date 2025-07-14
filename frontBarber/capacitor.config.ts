803cec3b33997293d9cded574b788c0533e7b334
Sergio-P23 <patarroyoarias23@gmail.com>
2025-07-14 18:42:55 -0500

    borrar

--- frontBarber/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'frontBarber',
-  webDir: 'www'
-};
-
-export default config;
