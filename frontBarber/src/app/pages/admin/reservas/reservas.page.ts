047df32d1c62e391699cea68d4c014dc87d2ec4e
Sergio-P23 <patarroyoarias23@gmail.com>
2025-07-21 19:42:52 -0500

    iniciacion vista admin/reservas

--- frontBarber/src/app/pages/admin/reservas/reservas.page.ts
+++ frontBarber/src/app/pages/admin/reservas/reservas.page.ts
@@ -1,18 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {  IonButtons, IonContent, IonHeader, IonMenu, IonMenuButton, IonTitle, IonToolbar, } from '@ionic/angular/standalone';
+
+import { IonicModule } from '@ionic/angular'; // Este importa TODO lo de Ionic
+
+import { addIcons } from 'ionicons'; // Importa addIcons
+import { menuOutline, logOutOutline } from 'ionicons/icons';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-reservas',
   templateUrl: './reservas.page.html',
   styleUrls: ['./reservas.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    CommonModule,
+    IonicModule,
+    RouterModule,]
 })
 export class ReservasPage implements OnInit {
 
-  constructor() { }
+  constructor() {
+    addIcons({
+      menuOutline, // Icono de hamburguesa
+      logOutOutline
+        });
+   }
 
   ngOnInit() {
   }
