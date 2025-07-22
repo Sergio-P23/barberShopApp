d7987646bef92a1bcb39884a5e00601a1d9cb341
Sergio-P23 <patarroyoarias23@gmail.com>
2025-07-21 21:00:47 -0500

    vista admin/reservas funcional

--- frontBarber/src/app/pages/admin/reservas/reservas.page.ts
+++ frontBarber/src/app/pages/admin/reservas/reservas.page.ts
@@ -2,12 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FormsModule } from '@angular/forms';
-import {  IonButtons, IonContent, IonHeader, IonMenu, IonMenuButton, IonTitle, IonToolbar, } from '@ionic/angular/standalone';
+import { IonButtons, IonContent, IonHeader, IonMenu, IonMenuButton, IonTitle, IonToolbar, } from '@ionic/angular/standalone';
 
 import { IonicModule } from '@ionic/angular'; // Este importa TODO lo de Ionic
 
 import { addIcons } from 'ionicons'; // Importa addIcons
-import { menuOutline, logOutOutline } from 'ionicons/icons';
+import { menuOutline, logOutOutline, calendarOutline } from 'ionicons/icons';
 import { RouterModule } from '@angular/router';
 
 @Component({
@@ -18,18 +18,90 @@ import { RouterModule } from '@angular/router';
   imports: [
     CommonModule,
     IonicModule,
-    RouterModule,]
+    RouterModule,
+    FormsModule,]
 })
 export class ReservasPage implements OnInit {
 
   constructor() {
     addIcons({
       menuOutline, // Icono de hamburguesa
-      logOutOutline
-        });
-   }
+      logOutOutline,
+      calendarOutline
+    });
+  }
 
   ngOnInit() {
   }
+
+  reservas = [
+    {
+      titulo: 'Corte Clásico',
+      fecha: '2025-07-01',
+      nombre: 'Pedro Gómez',
+      telefono: '3001234567',
+      barbero: 'Carlos',
+      precio: 25000
+    },
+    {
+      titulo: 'Cejas',
+      fecha: '2025-08-01',
+      nombre: 'Laura Ríos',
+      telefono: '3123456789',
+      barbero: 'Juan',
+      precio: 10000
+    },
+    {
+      titulo: 'Corte + Barba',
+      fecha: '2025-08-03',
+      nombre: 'David Torres',
+      telefono: '3012345678',
+      barbero: 'Carlos',
+      precio: 35000
+    },
+    {
+      titulo: 'Color',
+      fecha: '2025-08-04',
+      nombre: 'Julián Ortega',
+      telefono: '3045678910',
+      barbero: 'Juan',
+      precio: 45000
+    }
+  ];
+
+  reservasFiltradas = [...this.reservas]; // inicializa con todas
+  fechaFiltro: string = '';
+  mostrarModalBorrar = false;
+  reservaSeleccionada: any;
+
+  minDate: string = '2020-01-01'; // Fecha mínima
+  maxDate: string = '2030-12-31'; // Fecha máxima
+
+
+   confirmarCancelacion(reserva: any) {
+    this.reservaSeleccionada = reserva;
+    this.mostrarModalBorrar = true;
+  }
+
+  cancelar() {
+    this.reservas = this.reservas.filter(r => r !== this.reservaSeleccionada);
+    this.filtrarPorFecha(); // volver a aplicar el filtro
+    this.mostrarModalBorrar = false;
+  }
+
+  filtrarPorFecha() {
+    console.log('Fecha seleccionada:', this.fechaFiltro); // Para debug
+
+    if (this.fechaFiltro) {
+      const fechaSeleccionada = new Date(this.fechaFiltro);
+      const fechaStr = fechaSeleccionada.toISOString().split('T')[0]; // Formato YYYY-MM-DD
+
+      this.reservasFiltradas = this.reservas.filter(r => r.fecha === fechaStr);
+    } else {
+      this.reservasFiltradas = [...this.reservas];
+    }
+  }
+
+ 
 
 }
